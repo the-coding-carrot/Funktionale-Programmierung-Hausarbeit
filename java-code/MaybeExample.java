0d06c89efaf908453b8f95a9044e5c708697b100
heofthetea <heofthetea@gmail.com>
2025-12-09 11:54:37 +0100

    same as before

--- java-code/MaybeExample.java
+++ java-code/MaybeExample.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class MaybeExample {
 
+    /**
+     * Parse an Integer into a Maybe<Integer>
+     */
     private static Maybe<Integer> parseMaybe(String s) {
         if (s.matches("\\d+")) {
             return Maybe.unit(Integer.parseInt(s));
@@ -10,29 +13,31 @@ public class MaybeExample {
         }
     }
 
+    /**
+     * Divide numerator by denominator if it is cleanly divisible.
+     * @return Just<Integer> if division is possible, Nothing if it is not
+     */
+    private static Maybe<Integer> divideBy(Integer numerator, Integer denominator) {
+        if (denominator == 0 || numerator % denominator != 0) {
+            return Maybe.nothing();
+        }
+        return Maybe.unit(numerator / denominator);
+    }
+
     public static void main(String[] args) {
-        // Example 1: Simple chaining with valid values
-        int val = 4;
-        Maybe<String> result = Maybe.unit(val)
-        .bind(x -> Maybe.unit(x - 2))
-        .bind(x -> Maybe.unit(String.valueOf(x)));
-
-        assert result.getValue().equals("2");
-        System.out.println("Example 1 result: " + result);
-
-        // Example 2: Handling user input that might be invalid
         System.out.println("\nExample 2: Enter a number:");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
         Maybe<Integer> result2 = Maybe.unit(input)
-                .bind(MaybeExample::parseMaybe)
-                .bind(x -> Maybe.unit(x - 2));
+            .bind(MaybeExample::parseMaybe)
+            .map(x -> x - 2)
+            .bind(x -> MaybeExample.divideBy(x, 1));
 
         if (result2.isPresent()) {
             System.out.println("Result: " + result2.getValue());
         } else {
-            System.out.println("Error: Invalid input, could not parse as number");
+            System.out.println("Some Error occured (use a result monad if you want to know what exactly went wrong)");
         }
 
         scanner.close();
